85807f21170c204a1b6f756bd24744bab3b212ab
Matt Mower <mowerm@gmail.com>
2017-01-02 02:26:01 -0600

    envsetup: Fix check for deprecated CM device tree
    By the time lunch is run on a product name, it's too late to check
    whether a deprecated CM device tree is being used (roomservice runs in
    lunch). Instead, rely on check_product() to identify whether a tree
    uses cm_ or lineage_ before running lunch.
    
    Conflicts:
    	build/envsetup.sh
    Change-Id: I16288365f791ff68c3226f3808427168b2fc06fa

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -63,7 +63,13 @@ function breakfast()
             if [ -z "$variant" ]; then
                 variant="userdebug"
             fi
-            lunch uos_$target-$variant
+
+            if ! check_product lineage_$target && check_product cm_$target; then
+                echo "** Warning: '$target' is using CM-based makefiles. This will be deprecated in the next major release."
+                lunch cm_$target-$variant
+            else
+                lunch lineage_$target-$variant
+            fi
         fi
     fi
     return $?
